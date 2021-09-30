fb9acccdd78d97a114df26600b0e6a6604ec5019
SteakFisher <88953813+SteakFisher@users.noreply.github.com>
2021-09-30 20:19:25 +0400

    Add files via upload

---
+++ src/main/java/me/steakfisher/cowsmp/CowSMP.java
@@ -0,0 +1,29 @@
+package me.steakfisher.cowsmp;
+
+import me.steakfisher.cowsmp.listeners.onDeathEvent;
+import me.steakfisher.cowsmp.listeners.onJoinEvent;
+import me.steakfisher.cowsmp.listeners.onRespawnEvent;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CowSMP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        loadConfig();
+        getServer().getPluginManager().registerEvents((Listener) new onJoinEvent(), this);
+        getServer().getPluginManager().registerEvents((Listener) new onDeathEvent(), this);
+        getServer().getPluginManager().registerEvents((Listener) new onRespawnEvent(), this);
+    }
+
+    public void loadConfig(){
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
